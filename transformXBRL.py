afad3c41e030cc85821ff0786d4f85ee27ccce77
NSO-XBRL <130356018+NSO-XBRL@users.noreply.github.com>
2023-04-21 14:48:28 +0300

    начинаем трансформить xbrl в xml

--- transformXBRL.py
+++ transformXBRL.py
@@ -1,5 +1,4 @@
 import itertools,collections,xmltodict,json,xml.dom.minidom as minidom,xml.etree.ElementTree as ET
-
 import numpy as np
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
@@ -42,6 +41,7 @@ class transformxml():
         varible_data={}
         for context in contexts:
             context_data[context['@id']]={}
+            td_dict = []
             if 'xbrldi:explicitMember' in context['xbrli:scenario'].keys():
                 ex_dict=[]
                 if type(context['xbrli:scenario']['xbrldi:explicitMember'])==list:
@@ -50,15 +50,25 @@ class transformxml():
                 else:
                     ex_dict.append(context['xbrli:scenario']['xbrldi:explicitMember']['@dimension'] + '|' + context['xbrli:scenario']['xbrldi:explicitMember']['#text'])
                 context_data[context['@id']]['axis']=ex_dict
+
             if 'xbrldi:typedMember' in context['xbrli:scenario'].keys():
-                for td_key in context['xbrli:scenario']['xbrldi:typedMember'].keys():
-                    if '@' not in td_key:
-                        domain_name=td_key
-                taxis=context['xbrli:scenario']['xbrldi:typedMember']['@dimension']
-                taxis_domain=taxis+'|'+domain_name
-                taxis_value=context['xbrli:scenario']['xbrldi:typedMember'][domain_name]
-                context_data[context['@id']]['taxis']= taxis_domain
-                context_data[context['@id']]['taxis_value'] = taxis_value
+                if type(context['xbrli:scenario']['xbrldi:typedMember']) == list:
+                    for td_key in context['xbrli:scenario']['xbrldi:typedMember'].keys():
+                        if '@' not in td_key:
+                            domain_name=td_key
+                        taxis=context['xbrli:scenario']['xbrldi:typedMember']['@dimension']
+                        taxis_domain=taxis+'|'+domain_name+'|'+context['xbrli:scenario']['xbrldi:typedMember'][domain_name]
+                        td_dict.append(taxis_domain)
+                    context_data[context['@id']]['taxis']= td_dict
+                else:
+                    for td_key in context['xbrli:scenario']['xbrldi:typedMember'].keys():
+                        if '@' not in td_key:
+                            domain_name = td_key
+                    taxis = context['xbrli:scenario']['xbrldi:typedMember']['@dimension']
+                    taxis_domain = taxis + '|' + domain_name + '|' + context['xbrli:scenario']['xbrldi:typedMember'][
+                        domain_name]
+                    td_dict.append(taxis_domain)
+            context_data[context['@id']]['taxis'] = td_dict
         # for xx in context_data.keys():
         #     print(xx,context_data[xx])
         # print('------------------------------------------------------------------------------------------------------')
@@ -84,12 +94,20 @@ class transformxml():
                         razdel_axis.append(axis_synt_value_final)
             razdel_axis=self.only_axis(razdel_axis)
             razdel_axis.sort()
+
+            razdel_taxis=[]
+            for taxis_key in data_dict[razdel_key]['taxis'].keys():
+                razdel_taxis.append(data_dict[razdel_key]['taxis'][taxis_key])
+            razdel_taxis.sort()
+
             for varible_key in data_dict[razdel_key]['varible'].keys():
-                varible_axis=self.only_axis(data_dict[razdel_key]['varible'][varible_key]['axis'])
-                data_list_reverse.append({'var':data_dict[razdel_key]['varible'][varible_key]['var'],
+                # varible_axis=self.only_axis(data_dict[razdel_key]['varible'][varible_key]['axis'])
+                data_list_reverse.append({'razdel':razdel_key,'var':data_dict[razdel_key]['varible'][varible_key]['var'],
                                           'razdel_axis': razdel_axis,
                                           'varible_axis': data_dict[razdel_key]['varible'][varible_key]['axis'],
+                                          'taxis':razdel_taxis,
                                           'name':varible_key})
+
         # for xx in data_list_reverse:
         #     print(xx)
         final_data=[]
@@ -99,6 +117,7 @@ class transformxml():
                 value=varible_data[var][cont]
                 axis=context_data[cont].get('axis')
                 axis.sort()
+                taxis=context_data[cont].get('taxis')
                 only_axis=self.only_axis(axis)
                 only_axis.sort()
                 for xx in data_list_reverse:
@@ -106,17 +125,32 @@ class transformxml():
                         #final_data.append([list(set(axis) - set(xx['varible_axis'])),var,context,xx['name'],value])
                         temp_razde_axis=list(set(axis) - set(xx['varible_axis']))
                         temp_razde_axis.sort()
-                        final_data.append({'razdel_axis':temp_razde_axis,'var':var,'context':context,'name':xx['name'],'value':value})
-                        #print(list(set(axis) - set(xx['varible_axis'])),var,context,xx['name'],value)
+                        final_data.append({'razdel':xx['razdel'],'razdel_axis':temp_razde_axis,'razdel_taxis':taxis,'var':var,'context':context,'name':xx['name'],'value':value})
+                        #print(var, context, [tt.split('|')[0]+tt.split('|')[1] for tt in taxis],xx['taxis'])
                     elif xx['var']==var and numpy.isin(xx['razdel_axis'],only_axis).all() and numpy.isin(only_axis,xx['razdel_axis']).all() and xx['varible_axis']==None:
                         #final_data.append([axis, var, context, xx['name'], value])
-                        final_data.append({'razdel_axis': axis,'var':var,'context':context,'name':xx['name'],'value':value})
-                        #print(axis,var, context, xx['name'],value)
-        line_group=[xx.get('razdel_axis') for xx in final_data]
-        for group_axis in self.sort_and_deduplicate(line_group):
+                        final_data.append({'razdel':xx['razdel'],'razdel_axis': axis,'razdel_taxis':taxis, 'var':var,'context':context,'name':xx['name'],'value':value})
+                        #print(var, context, [tt.split('|')[0]+tt.split('|')[1] for tt in taxis],xx['taxis'])
+
+        for xx in range(len(final_data)):
+            final_data[xx]['group_osi']=final_data[xx].get('razdel_axis')+final_data[xx].get('razdel_taxis')
+
+        # for xx in final_data:
+        #     print(xx)
+
+        line_group=[xx.get('group_osi') for xx in final_data]
+        line_group.sort()
+        result_otchet= {}
+        for group_osi in self.sort_and_deduplicate(line_group):
+            result_otchet[str(group_osi)]={}
             for xx in final_data:
-                if xx['razdel_axis']==group_axis:
-                    print(group_axis,xx['var'],xx['name'],xx['context'])
+                if numpy.isin(xx['group_osi'],group_osi).all() and numpy.isin(group_osi,xx['group_osi']).all():
+                    result_otchet[str(group_osi)]['razdel'] = xx['razdel']
+                    result_otchet[str(group_osi)][xx['name']] = xx['value']
+
+
+        for xx in result_otchet.keys():
+            print(xx,result_otchet[xx])
 
 if __name__ == "__main__":
     ss=transformxml('mapping_0409725_old.json')
